01666d2afffced73d991d0c6e365723980d855ed
HhDreamer <hh.dreamer@gmail.com>
2023-03-18 23:19:31 +0100

    commit add schemas

---
+++ schemas/bourse.js
@@ -0,0 +1,37 @@
+export default {
+  name: 'bourse',
+  type: 'document',
+  title: 'Bourse',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Titre',
+    },
+    {
+      name: 'paragraphs',
+      type: 'array',
+      title: 'Paragraphes',
+      of: [
+        {
+          name: 'paragraph',
+          type: 'object',
+          title: 'Paragraphe',
+          fields: [
+            {
+              name: 'title',
+              type: 'string',
+              title: 'Sujet',
+            },
+            {
+              name: 'text',
+              type: 'array',
+              title: 'Texte',
+              of: [{type: 'block'}],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,7 @@
+import bourse from './bourse'
 import social from './social'
 import logement from './logement'
 import cvec from './cvec'
+import autre from './autre'
 
-export const schemaTypes = [social, logement, cvec]
+export const schemaTypes = [bourse, social, logement, cvec, autre]
